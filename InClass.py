495b2c3e0285a68934c2076addea985993bde32a
ewhart1 <ewhart1@lsu.edu>
2018-10-11 11:58:10 -0700

    Week 8 in class DNA compliment and reverse

---
+++ InClass.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# script to reverse the seqeuence of a sring of DNA and to generate the complement
+
+
+# insert a DNA sequence
+
+myString = input("DNA sequence here:")
+
+# This command will convert the characters to lower case in order to delienate differences
+myString = myString.lower()
+# Next 4 commands replace the original nucleotide with its compliment
+myString = myString.replace("g", "C")
+
+myString = myString.replace("c", "G")
+
+myString = myString.replace("a", "T")
+
+myString = myString.replace("t", "A")
+
+# print the inverse of compliment sequence
+print(myString[::-1])
